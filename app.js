56235c9817cc2140313bb5f36aedf7f951bb79e0
FILIPE MOTA DA SILVA LOURENCO <a92800494@rn.senac.local>
2024-11-28 19:28:49 -0300

    rota usuarios

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Olá, Docker!');
+app.get("/", (req, res) => {
+  res.send("Olá, Docker!");
+});
+
+app.get("/usuarios", (req, res) => {
+  const usuarios = [
+    { id: 1, nome: "João Silva", email: "joao.silva@example.com" },
+    { id: 2, nome: "Maria Oliveira", email: "maria.oliveira@example.com" },
+    { id: 3, nome: "Carlos Santos", email: "carlos.santos@example.com" },
+  ];
+  res.json(usuarios);
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
